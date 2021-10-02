1f243d81629d3911b991a3237826538d349965c9
Amanda Barboza <amanda.brbz@hotmail.com>
2021-10-02 17:35:51 +0000

    exibe botoes a partir do service

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -3,6 +3,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Turma } from 'src/app/models/turma';
 import { Disciplina } from 'src/app/models/disciplina';
 import { Aluno } from 'src/app/models/aluno';
+import { TurmaService } from 'src/app/services/turma.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-turmas',
@@ -11,40 +13,15 @@ import { Aluno } from 'src/app/models/aluno';
 })
 export class ListaTurmasComponent implements OnInit {
 
+  public turmas$: Observable<Turma[]>;
+
   @Output()
   public turmaSelecionada: EventEmitter<Turma> = new EventEmitter();
 
-  public turmas: Turma[] = [
-    new Turma(
-      new Disciplina(
-        'WEB01',
-        'Fundamentos do Desenvolvimento Web',
-      ),
-      2021,
-      1,
-      [
-        new Aluno(1234, 'João da Silva'),
-        new Aluno(1235, 'Maria Joana Pereira'),
-        new Aluno(1231, 'Oswaldo Siqueira'),
-      ],
-    ),
-    new Turma(
-      new Disciplina(
-        'WEB02',
-        'Introdução aos Frameworks',
-      ),
-      2021,
-      2,
-      [
-        new Aluno(4567, 'Antonio Jose da Silva'),
-        new Aluno(4563, 'Jose Abreu'),
-        new Aluno(4565, 'Katarina Couves'),
-      ],
-    ),
-  ];
-
   constructor(
-  ) {
+    private turmaService: TurmaService,
+    ) {
+      this.turmas$ = this.turmaService.get();
   }
 
   ngOnInit(): void {
--- src/app/services/turma.service.ts
+++ src/app/services/turma.service.ts
@@ -1,11 +1,69 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Turma } from '../models/turma'
+import { Aluno } from '../models/aluno'
+import { Disciplina } from '../models/disciplina'
+
+const TURMAS = [
+  {
+    "id": "87382",
+    "disciplina": {
+      "codigo": "121",
+      "nome": "Fundamentos Web",
+    },
+    "ano": 2021,
+    "periodo": 1,
+    "alunos": [
+      {
+        "codigo": 1211,
+        "nome": "maria"
+      },
+    ],
+  },
+  {
+    "id": "45656",
+    "disciplina": {
+      "codigo": "1231d",
+      "nome": "Frameworks Web",
+    },
+    "ano": 2021,
+    "periodo": 2,
+    "alunos": [
+      {
+        "codigo": 1211,
+        "nome": "maria"
+      },
+    ],
+  },
+]
 @Injectable({
   providedIn: 'root'
 })
 export class TurmaService {
 
   constructor() {
-    
+
+  }
+
+  public get(): Observable<Turma[]> {
+    return of(TURMAS).pipe(
+      map(turmasRaw => turmasRaw.map(
+        turmaRaw => new Turma (
+          new Disciplina (
+            turmaRaw.disciplina.codigo,
+            turmaRaw.disciplina.nome
+          ),
+          turmaRaw.ano,
+          turmaRaw.periodo,
+          turmaRaw.alunos.map(raw => new Aluno (
+            raw.codigo,
+            raw.nome,
+          )),
+        )
+      )),
+    );
   }
 }
